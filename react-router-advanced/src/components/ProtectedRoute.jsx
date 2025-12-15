c72967522feeede2bbf0fec434be47f3df53e51b
Percerverence Nkuna <innocentiaperseverance@gmail.com>
2025-12-15 21:19:45 +0200

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,13 @@
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+
+const useAuth = () => {
   const isAuthenticated = localStorage.getItem("isAuth") === "true";
+  return { isAuthenticated };
+};
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
