bef56726a76b9617b5139c1408b7188d285869cb
Percerverence Nkuna <innocentiaperseverance@gmail.com>
2025-12-15 16:46:54 +0200

    Update RegistrationForm.jsx

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,73 +1,60 @@
 import { useState } from "react";
 
-
 const RegistrationForm = () => {
-const [formData, setFormData] = useState({
-username: "",
-email: "",
-password: "",
-});
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-const [error, setError] = useState("");
-const handleChange = (e) => {
-const { name, value } = e.target;
-setFormData({ ...formData, [name]: value });
+    if (!username || !email || !password) {
+      setError("All fields are required");
+      return;
+    }
+
+    setError("");
+    console.log("User Registered:", { username, email, password });
+
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Register (Controlled Form)</h2>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Register</button>
+    </form>
+  );
 };
-
-
-const handleSubmit = (e) => {
-e.preventDefault();
-
-
-if (!formData.username || !formData.email || !formData.password) {
-setError("All fields are required");
-return;
-}
-
-
-setError("");
-// Mock API call
-console.log("User Registered:", formData);
-
-
-setFormData({ username: "", email: "", password: "" });
-};
-
-
-return (
-<form onSubmit={handleSubmit}>
-<h2>Register (Controlled Form)</h2>
-
-
-{error && <p style={{ color: "red" }}>{error}</p>}
-<input
-type="text"
-name="username"
-placeholder="Username"
-value={formData.username}
-onChange={handleChange}
-/>
-<input
-type="email"
-name="email"
-placeholder="Email"
-value={formData.email}
-onChange={handleChange}
-/>
-
-
-<input
-type="password"
-name="password"
-placeholder="Password"
-value={formData.password}
-onChange={handleChange}
-/>
-<button type="submit">Register</button>
-</form>
-);
-};
-
 
 export default RegistrationForm;
